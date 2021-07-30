d57bdb3d6aaca7b412a31d339acd517e6ce623af
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2021-07-30 13:32:07 +0300

    heapsort algorithm

--- CormenBook/sorting_algorithms/heapsort.cpp
+++ CormenBook/sorting_algorithms/heapsort.cpp
@@ -5,7 +5,32 @@
 #include "heapsort.h"
 
 #include <iostream>
+#include <cassert>
 
 void testHeapsort() {
+    std::vector<int> v1 {5, 13, 2, 25, 7, 17, 20, 8, 4};
+    std::vector<int> r1 {2, 4, 5, 7, 8, 13, 17, 20, 25};
+
+    heapsort(v1);
+    assert(v1 == r1);
+
+    std::vector<int> v2 {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    std::vector<int> r2 {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    heapsort(v2);
+    assert(v2 == r2);
+
+    std::vector<int> v3 {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> r3 {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    heapsort(v3);
+    assert(v3 == r3);
+
+    std::vector<std::string> v4 {"dreptunghiul", "este", "un", "paralelogram", "cu", "un", "unghi", "drept"};
+    std::vector<std::string> r4 {"cu", "drept", "dreptunghiul", "este", "paralelogram", "un", "un", "unghi"};
+
+    heapsort(v4);
+    assert(v4 == r4);
+
     std::cout << "...testing heapsort algorithm\n";
 }
--- CormenBook/sorting_algorithms/heapsort.h
+++ CormenBook/sorting_algorithms/heapsort.h
@@ -14,13 +14,48 @@
 #include <vector>
 
 template <class T>
+void buildMaxHeap(std::vector<T>& data);
+
+template <class T>
+void maxHeapify(std::vector<T>& data, int size, int index);
+
+template <class T>
 void heapsort(std::vector<T>& data);
 
 void testHeapsort();
 
 template<class T>
-void heapsort(std::vector<T> &data) {
+void buildMaxHeap(std::vector<T> &data) {
+    for (int i = (data.size() - 1) / 2; i >= 0; --i)
+        maxHeapify(data, data.size(), i);
+}
 
+template<class T>
+void maxHeapify(std::vector<T> &data, int size, int index) {
+    while (index < size) {
+        int right = index * 2 + 1, left = index * 2 + 2, maximum = index;
+
+        if (left < size && data[maximum] < data[left])
+            maximum = left;
+        if (right < size && data[maximum] < data[right])
+            maximum = right;
+
+        std::swap(data[index], data[maximum]);
+        if (index == maximum) break;
+        index = maximum;
+    }
+}
+
+template<class T>
+void heapsort(std::vector<T> &data) {
+    buildMaxHeap(data);
+    int size = data.size();
+
+    for (int i = size - 1; i >= 1; --i) {
+        std::swap(data[i], data[0]);
+        size--;
+        maxHeapify(data, size, 0);
+    }
 }
 
 #endif //CORMENBOOK_HEAPSORT_H
